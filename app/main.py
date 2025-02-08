70cf0c078e0fe5ca34537f8ab0bc203f56ecab60
Seb <jssaavedraa@unal.edu.co>
2025-02-08 09:28:02 -0500

    Update: Multiples modelos

--- app/main.py
+++ app/main.py
@@ -12,31 +12,68 @@ class IrisInput(BaseModel):
     sepal_width: float
     petal_length: float
     petal_width: float
+    model: str  
 
-#Se carga el modelo 
-try:
-    with open("model.pkl", "rb") as f:
-        model = pickle.load(f)
-except Exception as e:
-    raise RuntimeError(f"Error al cargar el modelo: {e}")
 
 #Se genera el decorador por modelo
-@app.post("/predict/model1")
+@app.post("/predict")
 
 async def predict(input_data: IrisInput):
-    try:
-        # Tratamiento de información
-        data = np.array([
-            input_data.sepal_length,
-            input_data.sepal_width,
-            input_data.petal_length,
-            input_data.petal_width
-        ]).reshape(1, -1)
 
-        # Prediccion del modelo
-        prediction = model.predict(data)
-        # Convertir la predicción a entero (ya que scikit-learn retorna un array)
-        result = int(prediction[0])
-        return {"prediction": result}
-    except:
-        return {"prediction": "no valido"}
+    ### Predicción modelo 1
+    if input_data.model =="model1":
+
+        #Se carga el modelo 
+        try:
+            with open("model.pkl", "rb") as f:
+                model = pickle.load(f)
+        except Exception as e:
+            raise RuntimeError(f"Error al cargar el modelo: {e}")
+
+
+        try:
+            # Tratamiento de información
+            data = np.array([
+                input_data.sepal_length,
+                input_data.sepal_width,
+                input_data.petal_length,
+                input_data.petal_width
+            ]).reshape(1, -1)
+
+            # Prediccion del modelo
+            prediction = model.predict(data)
+            # Convertir la predicción a entero (ya que scikit-learn retorna un array)
+            result = int(prediction[0])
+            return {"prediction": result}
+        except:
+            return {"prediction": "no valido"}
+
+    ### Predicción modelo 2    
+    elif input_data.model =="model2":
+
+        #Se carga el modelo 
+        try:
+            with open("model.pkl", "rb") as f:
+                model = pickle.load(f)
+        except Exception as e:
+            raise RuntimeError(f"Error al cargar el modelo: {e}")
+
+        try:
+            # Tratamiento de información
+            data = np.array([
+                input_data.sepal_length,
+                input_data.sepal_width,
+                input_data.petal_length,
+                input_data.petal_width
+            ]).reshape(1, -1)
+
+            # Prediccion del modelo
+            prediction = model.predict(data)
+            # Convertir la predicción a entero (ya que scikit-learn retorna un array)
+            result = int(prediction[0])
+            return {"prediction": result}
+        except:
+            return {"prediction": "no valido"}
+        
+    else: 
+        return {"prediction": "modelo no valido"}
