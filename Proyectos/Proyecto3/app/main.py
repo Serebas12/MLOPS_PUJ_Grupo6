fc16fc7188e8c74d62707e87af33e16784e75dc7
Seb <jssaavedraa@unal.edu.co>
2025-04-26 11:32:07 -0500

    Update jupyter

--- Proyectos/Proyecto3/app/main.py
+++ Proyectos/Proyecto3/app/main.py
@@ -9,7 +9,7 @@ import boto3
 
 #Inicia la aplicación
 app = FastAPI(
-    title="Covertype Predictor API – ML Model Serving",
+    title="Predictor API – ML Model Serving",
     description="Realizar predicciones usando el modelo más reciente en stage Production",
     version="1.0.0"
 )
@@ -43,44 +43,32 @@ async def list_models():
 
 
 #Datos requeridos de la solicitud
-class CoverTypeInput(BaseModel):
-    Elevation: float
-    Aspect: float
-    Slope: float
-    Horizontal_Distance_To_Hydrology: float
-    Vertical_Distance_To_Hydrology: float
-    Horizontal_Distance_To_Roadways: float
-    Hillshade_9am: float
-    Hillshade_Noon: float
-    Hillshade_3pm: float
-    Horizontal_Distance_To_Fire_Points: float
-    Wilderness_Area: str
-    Soil_Type: str
-    model: str
+class ModelInput(BaseModel):
+    island: str	
+    culmen_length_mm: float	
+    culmen_depth_mm: float
+    flipper_length_mm: float	
+    body_mass_g: float	
+    sex: str
+    model: str	
 
 #Se genera el decorador por modelo
 @app.post("/predict")
 
-async def predict(input_data: CoverTypeInput):
+async def predict(input_data: ModelInput):
 
     """
-    Puede realizar la predicción del tipo de cobertura forestal (Cover_Type) utilizando uno de los modelos preentrenados registrados en MLflow.
+    Puede realizar la predicción de la especie de un pingüino, seleccionando uno de los modelos pre entrenados.
+    
+    - **island**: Isla a la que pertenece el pingüino. Valores validos Biscoe, Dream y Torgersen.
+    - **sex**: Sexo del pingüinos. Valores validos MALE, FEMALE.
+    - **culmen_length_mm**: Longitud del culmen en mm.
+    - **culmen_depth_mm**: Profundidad del culmen en mm.
+    - **flipper_length_mm**: Profundidad de la aleta en mm.
+    - **body_mass_g**: Masa corporal en mm.
+    - **model**: Modelo que se desea utilizar. 
 
-    -   Elevation: Elevación del terreno en metros.
-    -   Aspect: Orientación del terreno en grados (0 a 360).
-    -   Slope: Inclinación del terreno en grados.
-    -   Horizontal_Distance_To_Hydrology: Distancia horizontal a cuerpos de agua en metros.
-    -   Vertical_Distance_To_Hydrology: Distancia vertical a cuerpos de agua en metros.
-    -   Horizontal_Distance_To_Roadways: Distancia horizontal a carreteras en metros.
-    -   Hillshade_9am: Nivel de sombra proyectada a las 9:00 AM (0 a 255).
-    -   Hillshade_Noon: Nivel de sombra proyectada al mediodía (0 a 255).
-    -   Hillshade_3pm: Nivel de sombra proyectada a las 3:00 PM (0 a 255).
-    -   Horizontal_Distance_To_Fire_Points: Distancia horizontal a zonas de fuego controlado en metros.
-    -   Wilderness_Area: Área silvestre a la que pertenece. Valores válidos: Rawah, Neota, Comanche Peak, Cache la Poudre.  
-    -   Soil_Type: Tipo de suelo categorizado. Valores válidos: Soil_1 a Soil_40.
-    -   model: Modelo que se desea utilizar para la predicción. 
-
-    Devuelve un JSON con la clase de cobertura forestal estimada (Cover_Type), codificada del 1 al 7 según la clasificación del Servicio Forestal de EE.UU.
+    Devuelve un JSON con la predicción.
     """
 
     ### Predicción modelo 1
@@ -88,20 +76,13 @@ async def predict(input_data: CoverTypeInput):
     try:
         # Tratamiento de información
         data = pd.DataFrame([{
-            'Elevation': int(input_data.Elevation), 
-            'Aspect': int(input_data.Aspect), 
-            'Slope': int(input_data.Slope), 
-            'Horizontal_Distance_To_Hydrology': int(input_data.Horizontal_Distance_To_Hydrology),
-            'Vertical_Distance_To_Hydrology': int(input_data.Vertical_Distance_To_Hydrology), 
-            'Horizontal_Distance_To_Roadways': int(input_data.Horizontal_Distance_To_Roadways),
-            'Hillshade_9am': int(input_data.Hillshade_9am), 
-            'Hillshade_Noon': int(input_data.Hillshade_Noon), 
-            'Hillshade_3pm': int(input_data.Hillshade_3pm),
-            'Horizontal_Distance_To_Fire_Points': int(input_data.Horizontal_Distance_To_Fire_Points), 
-            'Wilderness_Area': input_data.Wilderness_Area, 
-            'Soil_Type': input_data.Soil_Type
+            "island": input_data.island,
+            "sex": input_data.sex,
+            "culmen_length_mm": input_data.culmen_length_mm,
+            "culmen_depth_mm": input_data.culmen_depth_mm,
+            "flipper_length_mm": input_data.flipper_length_mm,
+            "body_mass_g": input_data.body_mass_g
         }])
-
 
         os.environ['MLFLOW_S3_ENDPOINT_URL'] = "http://minio:9000"
         os.environ['AWS_ACCESS_KEY_ID'] = 'admin'
@@ -119,7 +100,6 @@ async def predict(input_data: CoverTypeInput):
         predict = loaded_model.predict(data)
         resultado = predict[0]
 
-        resultado = int(resultado)
 
         return {"prediction": resultado}
     except Exception as e:
